e3c4251356710209138212f0a2ee9ae97145c9bf
Uelinton Janke <uelinton.h@gmail.com>
2025-09-15 22:48:53 -0300

    feat: adição das imagens da página de login

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,34 @@
 "use client";
 
-import Image from "next/image"; 
+import Image from "next/image";
 import { signIn } from "next-auth/react";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-black text-white font-sans"> 
+    <main className="relative flex min-h-screen flex-col items-center justify-start pt-35 text-white font-sans">
+      {/* Background */}
+      <div className="absolute inset-0 -z-10">
+        <div className="relative w-full h-full">
+          <Image
+            src="/background.png"
+            alt="Background"
+            fill
+            priority
+            quality={100}
+            className="object-cover"
+          />
+        </div>
+      </div>
+
       {/* Logo */}
       <div className="flex flex-col items-center gap-6">
         <Image
-          src="/logo.svg" //colocar depois logo real
+          src="/logotipo.png"
           alt="Beatplay logo"
-          width={120}
-          height={120}
+          width={320}
+          height={320}
           priority
         />
-        <h1 className="text-3xl font-bold tracking-tight">Beatplay</h1> 
         <p className="text-gray-400 text-center max-w-sm">
           Entre com sua conta Spotify para começar a descobrir músicas de um jeito novo.
         </p>
