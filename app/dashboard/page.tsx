61ab08e13f8c4016b62029430acf8aa37efd7f67
Uelinton Janke <uelinton.h@gmail.com>
2025-09-18 23:08:56 -0300

    feat(dashboard): renomear função para DashboardPage e criar páginas internas com placeholders

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,7 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 
-export default async function Dashboard() {
+export default async function DashboardPage() {
     const session = await getServerSession(authOptions);
 
     return (
