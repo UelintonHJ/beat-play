92a38f6b94a9d86f32ca96357872552e6dc591a2
Uelinton Janke <uelinton.h@gmail.com>
2025-09-21 14:02:54 -0300

    refactor(dashboard): remover imagem duplicada de usuário e ajustar mensagem de boas-vindas

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,10 +6,10 @@ export default async function DashboardPage() {
 
     return (
         <div className="text-white p-8">
-            <h1>🎶 Bem-vindo ao Beatplay</h1>
+            <h1 className="text-2x1 font-bold mb-4">Bem-vindo ao Beatplay</h1>
             {session && (
                 <div>
-                    <p>Logado como {session.user?.name}</p>
+                    <p className="text-gray-300">Logado como <span className="font-semibold">{session.user?.name}</span> {session.user?.name}</p>
                     <img src={session.user?.image || ""} alt="Avatar" className="w-16 h-16"/>
                 </div>
             )}
