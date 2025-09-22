48301719a30c58531faaf48cd2c8ac513e268662
Uelinton Janke <uelinton.h@gmail.com>
2025-09-22 13:35:16 -0300

    chore(dashboard) remoção de console.log desnecessários

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -31,8 +31,6 @@ export default async function DashboardPage() {
     const session = await getServerSession(authOptions) as CustomSessions;
     const accessToken = session.accessToken;
 
-    console.log("Access Token Spotify:", accessToken);
-
     async function fetchSpotifyPlaylists(token: string): Promise<Playlist[]> {
         const res = await fetch("https://api.spotify.com/v1/me/playlists?limit=20", {
             headers: {
@@ -45,8 +43,6 @@ export default async function DashboardPage() {
         }
 
         const data = await res.json() as { items: SpotifyPlaylist[] };
-
-        console.log("Dados retornados pelo Spotify:", JSON.stringify(data, null, 2));
 
         return data.items.map((playlist) => ({
             id: playlist.id,
@@ -66,8 +62,6 @@ export default async function DashboardPage() {
             console.error(error);
         }
     }
-
-    console.log("Playlists recebidas do Spotify:", playlists);
 
     return (
         <div className="text-white p-8">
