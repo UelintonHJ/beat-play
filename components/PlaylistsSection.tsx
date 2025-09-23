7cb5c2463b422092c2d41206f3f746f026c7bead
Uelinton Janke <uelinton.h@gmail.com>
2025-09-23 09:30:20 -0300

    style(PlaylistsSection): remover hover verde dos ícones e intensificar glow verde da sombra

--- components/PlaylistsSection.tsx
+++ components/PlaylistsSection.tsx
@@ -60,9 +60,9 @@ export default function PlaylistsSection({ playlists }: PlaylistsSectionProps) {
                 {canScrollLeft && (
                     <button 
                         onClick={() => scroll("left")}
-                        className="group absolute left-0 top-1/2 transform -translate-y-1/2 bg-neutral-700 hover:bg-neutral-600 text-white w-10 h-10 flex items-center justify-center rounded-full z-10 transition-all shadow-md hover:shadow-[0_0_12px_rgba(16,185,129,1)]" 
+                        className="absolute left-0 top-1/2 transform -translate-y-1/2 bg-neutral-700 hover:bg-neutral-600 text-white w-10 h-10 flex items-center justify-center rounded-full z-10 transition-all shadow-md hover:shadow-[0_0_8px_rgba(16,185,129,1)]" 
                     >
-                        <ChevronLeft size={18} className="transition-colors group-hover:text-emerald-400"/>
+                        <ChevronLeft size={18} />
                     </button>
                 )}
 
@@ -88,9 +88,9 @@ export default function PlaylistsSection({ playlists }: PlaylistsSectionProps) {
                 {/* Botão para avançar */}
                 {canScrollRight && (
                     <button onClick={() => scroll("right")}
-                        className="group absolute right-0 top-1/2 transform -translate-y-1/2 bg-neutral-700 hover:bg-neutral-600 text-white w-10 h-10 flex items-center justify-center rounded-full z-10 transition-all shadow-md hover:shadow-[0_0_12px_rgba(16,185,129,1)]"
+                        className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-neutral-700 hover:bg-neutral-600 text-white w-10 h-10 flex items-center justify-center rounded-full z-10 transition-all shadow-md hover:shadow-[0_0_8px_rgba(16,185,129,1)]"
                     >
-                        <ChevronRight size={18} className="transition-colors group-hover:text-emerald-400"/>
+                        <ChevronRight size={18} />
                     </button>
                 )}
             </div>
