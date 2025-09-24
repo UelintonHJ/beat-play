10ee34111ba1213ed07a359331232a8815c824fa
Uelinton Janke <uelinton.h@gmail.com>
2025-09-24 14:25:07 -0300

    style(ArtistsSectionSkeleton): ajustar tamanho dos cards do skeleton para 194x224

--- components/ArtistsSectionSkeleton.tsx
+++ components/ArtistsSectionSkeleton.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { count } from "console";
-
 export default function ArtistsSectionSkeleton({ count = 5 }: { count?: number }) {
     return (
         <section className="mt-8">
@@ -9,7 +7,7 @@ export default function ArtistsSectionSkeleton({ count = 5 }: { count?: number }
             
             <div className="flex gap-4 overflow-x-hidden px-6">
                 {Array.from({ length: count }).map((_, idx) => (
-                    <div key={idx} className="w-32 h-40 flex-shrink-0 rounded-lg bg-neutral-800 shadow-md animate-pulse" />
+                    <div key={idx} className="w-[192px] h-[224px] flex-shrink-0 rounded-lg bg-neutral-800 shadow-md animate-pulse" />
                 ))}
             </div>
         </section>
