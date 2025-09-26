b825c5e09aab83e46164897c8d4bc8244f6ca897
Uelinton Janke <uelinton.h@gmail.com>
2025-09-26 17:16:41 -0300

    refactor(PlaylistsSection): aplicar HorizontalScrollSection

--- app/sections/PlaylistsSection.tsx
+++ app/sections/PlaylistsSection.tsx
@@ -1,31 +1,30 @@
 "use client";
 
 import PlaylistCard from "../../components/PlaylistCard";
-import { ChevronRight, ChevronLeft } from "lucide-react";
-import { useSmoothScroll } from "@/hooks/useSmoothScroll";
 import SectionSkeleton from "../../components/SectionSkeleton";
 import { useUserPlaylists } from "@/hooks/useUserPlaylists";
+import HorizontalScrollSection from "@/components/HorizontalScrollSection";
 
-interface PlaylistsSectionProps {
-    token: string
+interface Playlist {
+    id: string;
+    name: string;
+    owner: string;
+    image: string;
+    spotifyUrl: string;
 }
 
-export default function PlaylistsSection({ token}: PlaylistsSectionProps) {
+interface Props {
+    token: string;
+}
+
+export default function PlaylistsSection({ token }: PlaylistsSectionProps) {
     const {
         playlists,
         loading,
         error,
     } = useUserPlaylists(token);
 
-    const {
-        containerRef,
-        showLeftButton,
-        showRightButton,
-        scrollLeft,
-        scrollRight,
-    } = useSmoothScroll();
-
-    if(error) {
+    if (error) {
         return (
             <p className="text-red-500 mb-4">
                 Erro ao carregar playlists.
@@ -33,7 +32,7 @@ export default function PlaylistsSection({ token}: PlaylistsSectionProps) {
         );
     }
 
-    if(!playlists.length && !loading) {
+    if (!playlists.length && !loading) {
         return (
             <p className="text-gray-400 mb-4">
                 Nenhuma playlist encontrada no Spotify.
@@ -44,44 +43,21 @@ export default function PlaylistsSection({ token}: PlaylistsSectionProps) {
     return (
         <section>
             <h2 className="text-xl font-semibold mb-4">Minhas Playlists</h2>
-
-            <div className="relative">
-                {/* Botão de Voltar */}
-                {showLeftButton && (
-                    <button
-                        onClick={scrollLeft}
-                        className="absolute left-0 top-1/2 transform -translate-y-1/2 bg-neutral-700 hover:bg-neutral-600 text-white w-10 h-10 flex items-center justify-center rounded-full z-10 transition-all shadow-md"
-                    >
-                        <ChevronLeft size={18} />
-                    </button>
+            <HorizontalScrollSection>
+                {loading ? (
+                    <SectionSkeleton count={8} cardWidth="w-[192px]" cardHeight="h-[248px]" />
+                ) : (
+                    playlists.map((playlist) => (
+                        <PlaylistCard
+                            key={playlist.id}
+                            name={playlist.name}
+                            owner={playlist.owner}
+                            image={playlist.image}
+                            spotifyUrl={playlist.spotifyUrl}
+                        />
+                    ))
                 )}
-
-                {/* Container horizontal com overflow oculto */}
-                <div className="flex gap-4 overflow-x-hidden px-6" ref={containerRef}>
-                    {loading ? (
-                        <SectionSkeleton count={8} cardWidth="w-[192px]" cardHeight="h-[248px]" />
-                    ) : (
-                        playlists.map((playlist) => (
-                            <PlaylistCard
-                                key={playlist.id}
-                                name={playlist.name}
-                                owner={playlist.owner}
-                                image={playlist.image}
-                                spotifyUrl={playlist.spotifyUrl}
-                            />
-                        ))
-                    )}
-                </div>
-
-                {/* Botão para avançar */}
-                {showRightButton && (
-                    <button onClick={scrollRight}
-                        className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-neutral-700 hover:bg-neutral-600 text-white w-10 h-10 flex items-center justify-center rounded-full z-10 transition-all shadow-md"
-                    >
-                        <ChevronRight size={18} />
-                    </button>
-                )}
-            </div>
+            </HorizontalScrollSection>
         </section>
     );
 }
