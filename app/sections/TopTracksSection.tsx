2556c42058bebbb945f799c93d7b3b3c818a7daf
Uelinton Janke <uelinton.h@gmail.com>
2025-09-29 17:18:58 -0300

    style(TopTracksSection): adiciona hover no rounded-lg na sessão para destaque

--- app/sections/TopTracksSection.tsx
+++ app/sections/TopTracksSection.tsx
@@ -27,7 +27,7 @@ export default function TopTracksSection() {
                     tracks.map((track) => (
                         <div
                             key={track.id}
-                            className="flex-shrink-0 w-48 bg-neutral-900 rounded-lg p-2 hover:bg-neutral-800 hover:scale-105 transition-all duration-200 shadow-md"
+                            className="flex-shrink-0 w-48 bg-neutral-900 hover:rounded-lg p-2 hover:bg-neutral-800 hover:scale-105 transition-all duration-200 shadow-md"
                         >
                             <Image
                                 src={track.album.images[0]?.url}
