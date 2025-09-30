cd60a49166a1f60c8f6bfbaccf973ec87c818c6b
Uelinton Janke <uelinton.h@gmail.com>
2025-09-30 15:15:44 -0300

    feat(spotify): centralizar type em um só arquivo

---
+++ types/spotify.ts
@@ -0,0 +1,25 @@
+export interface Artist {
+    id: string;
+    name: string;
+    image?: string;
+    spotifyUrl?: string;
+}
+
+export interface Playlist {
+    id: string;
+    name: string;
+    owner: string;
+    image: string;
+    spotifyUrl: string;
+}
+
+export interface Track {
+    id: string;
+    name: string;
+    album: {
+        id?: string;
+        name?: string;
+        images: { url: string; width?: number; height?: number }[];
+    };
+    artists: Artist[];
+}
