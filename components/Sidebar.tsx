47895f975238d914141d3954d54cc2ad13f63e87
Uelinton Janke <uelinton.h@gmail.com>
2025-10-01 14:42:07 -0300

    fix(Sidebar): força logout completo do Spotify com redirect para o app

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -60,9 +60,9 @@ export default function Sidebar({ user }: SidebarProps) {
                     <span className="text-sm font-semibold">{user?.name}</span>
                     <button
                         onClick={() => signOut({ 
-                            redirect: true,
-                            callbackUrl: "https://accounts.spotify.com/logout", 
-                        })
+                            redirect: false }).then(() => {
+                                window.location.href = "https://accounts.spotify.com/logout?continue=https://beat-play-two.vercel.app";
+                            }) 
                     }
                         className="inline-block w-10 mt-2 text-xs bg-red-500 hover:bg-red-700 text-white-300 px-1 py-1 rounded-full transition cursor-pointer"
                     >
