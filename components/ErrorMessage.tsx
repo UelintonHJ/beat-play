d603c22ef7633931c0a0fbd389e4dfc0edc7fd14
Uelinton Janke <uelinton.h@gmail.com>
2025-10-01 09:58:36 -0300

    style(ErrorMessage): ajustar margin-top

--- components/ErrorMessage.tsx
+++ components/ErrorMessage.tsx
@@ -6,6 +6,6 @@ interface ErrorMessageProps {
 }
 export default function ErrorMessage({ message, className }: ErrorMessageProps) {
     return (
-        <p className={`flex justify-center text-red-500 mt-4 p-6 ${className || ""}`}>{message}</p>
+        <p className={`flex justify-center text-red-500 mt-6 p-6 ${className || ""}`}>{message}</p>
     );
 }
