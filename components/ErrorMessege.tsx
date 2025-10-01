79966413b277512b3f93e3fdb80a7ccfa48cb46f
Uelinton Janke <uelinton.h@gmail.com>
2025-10-01 09:41:03 -0300

    style(ErrorMessage): ajustar layout da frase com flex, justify-center e padding

--- components/ErrorMessege.tsx
+++ components/ErrorMessege.tsx
@@ -6,6 +6,6 @@ interface ErrorMessageProps {
 }
 export default function ErrorMessage({ message, className }: ErrorMessageProps) {
     return (
-        <p className={`text-red-500 mt-4 ${className || ""}`}>{message}</p>
+        <p className={`flex justify-center text-red-500 bg-black-300 rounded-full mt-4 p-6 ${className || ""}`}>{message}</p>
     );
 }
