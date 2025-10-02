e1ac22edc26530a9187c4afb25b0dbf34e4b37d5
Uelinton Janke <uelinton.h@gmail.com>
2025-10-02 14:28:24 -0300

    feat(hook): integrar getUserTopArtists

--- hooks/useRecommendations.ts
+++ hooks/useRecommendations.ts
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { getRecommendations } from "@/lib/spotify";
+import { getUserTopArtists, getRecommendationsFromTopArtists } from "@/lib/spotify";
 import { useSpotifyToken } from "./useSpotifyToken";
 import { Track } from "@/types/spotify";
 
@@ -15,21 +15,26 @@ export function useRecommendations(limit: number = 20) {
         setLoading(true);
         setError(null);
 
-        getRecommendations(token, limit)
-        .then((data) => {
-            const formattedTracks: Track[] = data.tracks.map((track: any) => ({
-                id: track.id,
-                name: track.name,
-                album: {
-                    ...track.album,
-                    images: track.album.images.lenght ? track.album.images : [{ url: "/track-mock.png" }],
-                },
-                artists: track.artists,
-            }));
-            setTracks(formattedTracks);
-        })
-        .catch((err) => setError(err.message || "Erro ao buscar recomendações"))
-        .finally(() => setLoading(false));
+        getUserTopArtists(token, 5)
+            .then((data) => {
+                const artistSeeds = data.items.mao((artists: any) => artistSeeds.id);
+                return getRecommendationsFromTopArtists(token, artistSeeds, limit);
+            })
+
+            .then((data) => {
+                const formattedTracks: Track[] = data.tracks.map((track: any) => ({
+                    id: track.id,
+                    name: track.name,
+                    album: {
+                        ...track.album,
+                        images: track.album.images.lenght ? track.album.images : [{ url: "/track-mock.png" }],
+                    },
+                    artists: track.artists,
+                }));
+                setTracks(formattedTracks);
+            })
+            .catch((err) => setError(err.message || "Erro ao buscar recomendações"))
+            .finally(() => setLoading(false));
     }, [token, limit]);
 
     return { tracks, loading, error };
