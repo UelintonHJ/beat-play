c5848198c0488c4c0273dfb15f32478e09228a32
Uelinton Janke <uelinton.h@gmail.com>
2025-10-06 14:13:48 -0300

    feat(components/ErrorMessage): add tipos unauthenticated e sessionExpired

--- components/ErrorMessage.tsx
+++ components/ErrorMessage.tsx
@@ -3,7 +3,7 @@
 interface ErrorMessageProps {
     message: string;
     className?: string;
-    type?: "error" | "offline" | "online";
+    type?: "error" | "offline" | "online" | "unauthenticated" | "sessionExpired";
 }
 
 export default function ErrorMessage({ message, type = "error", className }: ErrorMessageProps) {
@@ -13,11 +13,14 @@ export default function ErrorMessage({ message, type = "error", className }: Err
                 return "bg-yellow-400 text-black rounded-none";
             case "online":
                 return "bg-green-400 text-black rounded-none";
+            case "unauthenticated":
+            case "sessionExpired":
+                return "bg-orange-400 text-black rounded-none";
             case "error":
             default:
                 return "bg-red-400 text-black rounded-full px-6";
         }
-    }
+    };
 
     return (
         <div className={`flex justify-center items-center border py-2 mx-auto w-fit text-center shadow-sm font-semibold ${getColor()} ${className || ""}`}
