dbe2bc211445a372d93ba0fdbd0d20772955ffd4
Uelinton Janke <uelinton.h@gmail.com>
2025-10-06 11:36:01 -0300

    style(sections/PlaylistsSection): add margin-bottom à mensagem de erro para separar da próxima seção

--- app/sections/PlaylistsSection.tsx
+++ app/sections/PlaylistsSection.tsx
@@ -34,7 +34,7 @@ export default function PlaylistsSection() {
     return (
         <section>
             <h2 className="text-xl font-semibold mb-4">Minhas Playlists</h2>
-            {error && <ErrorMessage message="Erro ao carregar playlists." type="error"/>}
+            {error && <ErrorMessage message="Erro ao carregar playlists." type="error" className="mb-6"/>}
             <HorizontalScrollSection>
                 {loading ? (
                     <SectionSkeleton count={8} cardWidth="w-[192px]" cardHeight="h-[248px]" />
