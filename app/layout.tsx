891bd285765dbb1a0f9cb0fad3afb5c0a6b5793b
Uelinton Janke <uelinton.h@gmail.com>
2025-10-06 14:31:44 -0300

    feat(app/layout): add componente global para exibir status de autenticação

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from "./providers";
 import NetworkWatcher from "@/components/NetworkWatcher";
+import AuthWatcher from "@/components/AuthWatcher";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,6 +35,7 @@ export default function RootLayout({
       >
         <Providers>
           <NetworkWatcher />
+          <AuthWatcher />
           {children}
         </Providers>
       </body>
