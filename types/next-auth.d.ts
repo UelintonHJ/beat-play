ed5f4ef27273a96871cdb6abb20b8fe8164c2edf
Uelinton Janke <uelinton.h@gmail.com>
2025-10-06 16:38:19 -0300

    feat(types/next-auth.d.ts): adicionar o campo error

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -3,9 +3,19 @@ import NextAuth, { DefaultSession } from "next-auth";
 declare module "next-auth" {
     interface Session extends DefaultSession {
         accessToken?: string;
+        error?:string;
     }
 
     interface JWT {
         accessToken?: string;
+    }
+}
+
+declare module "next-auth/jwt" {
+    interface JWT {
+        accessToken?: string;
+        refreshToken?: string;
+        accessTokenExpires?: number;
+        error?: string;
     }
 }
