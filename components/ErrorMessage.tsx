1b82b3aad74d33a98ea2eeddf24a52ddd7b79069
Uelinton Janke <uelinton.h@gmail.com>
2025-10-06 10:51:17 -0300

    style(components/ErrorMessage): ajustar design dos banners online/offline com fundo colorido, texto preto, sem borda e sem arredondamento

--- components/ErrorMessage.tsx
+++ components/ErrorMessage.tsx
@@ -10,16 +10,16 @@ export default function ErrorMessage({ message, type = "error", className }: Err
     const getColor = () => {
         switch (type) {
             case "offline":
-                return "bg-yellow-100 text-yellow-700 border-yellow-400";
+                return "bg-yellow-400 text-black rounded-none";
             case "online":
-                return "bg-green-100 text-green-700 border-green-400";
+                return "bg-green-400 text-black rounded-none";
             default:
-                return "bg-red-100 text-red-700 border-red-400";
+                return "bg-red-100 text-red-700 border-xl";
         }
     }
 
     return (
-        <div className={`flex justify-center items-center border rounded-2xl mt-6 p-4 mx-auto w-fit text-center shadow-sm ${getColor()} ${className || ""}`}
+        <div className={`flex justify-center items-center border py-2 px-4 mx-auto w-fit text-center shadow-sm ${getColor()} ${className || ""}`}
         >
             {message}
             
