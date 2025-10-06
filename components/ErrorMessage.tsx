7af20e7e1eedda8773eee402c4b414d90a5923fb
Uelinton Janke <uelinton.h@gmail.com>
2025-10-06 08:37:19 -0300

    feat(components/ErrorMessage): adicionar prop type para personalizar cor e estilo conforme o tipo de mensagem

--- components/ErrorMessage.tsx
+++ components/ErrorMessage.tsx
@@ -3,9 +3,26 @@
 interface ErrorMessageProps {
     message: string;
     className?: string;
+    type?: "error" | "offline" | "online";
 }
-export default function ErrorMessage({ message, className }: ErrorMessageProps) {
+
+export default function ErrorMessage({ message, type = "error", className }: ErrorMessageProps) {
+    const getColor = () => {
+        switch (type) {
+            case "offline":
+                return "bg-yellow-100 text-yellow-700 border-yellow-400";
+            case "online":
+                return "bg-green-100 text-green-700 border-green-400";
+            default:
+                return "bg-red-100 text-red-700 border-red-400";
+        }
+    }
+
     return (
-        <p className={`flex justify-center text-red-500 mt-6 p-6 ${className || ""}`}>{message}</p>
+        <div className={`flex justify-center items-center border rounded-2xl mt-6 p-4 mx-auto w-fit text-center shadow-sm ${getColor} ${className || ""}`}
+        >
+            {message}
+            
+        </div>
     );
 }
