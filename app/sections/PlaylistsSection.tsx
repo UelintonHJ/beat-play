51bda6764d2177885beeeb39742a6f615faa20aa
Uelinton Janke <uelinton.h@gmail.com>
2025-10-06 11:25:49 -0300

    fix(sections/PlaylistsSection): mostrar título da seção mesmo quando há erro

--- app/sections/PlaylistsSection.tsx
+++ app/sections/PlaylistsSection.tsx
@@ -38,6 +38,7 @@ export default function PlaylistsSection() {
     return (
         <section>
             <h2 className="text-xl font-semibold mb-4">Minhas Playlists</h2>
+            {error && <ErrorMessage message="Erro ao carregar playlists." type="error"/>}
             <HorizontalScrollSection>
                 {loading ? (
                     <SectionSkeleton count={8} cardWidth="w-[192px]" cardHeight="h-[248px]" />
