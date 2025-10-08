5c5ef8ace077dd94a731d4ddd1d1fc1607075a9d
Uelinton Janke <uelinton.h@gmail.com>
2025-10-08 09:31:43 -0300

    feat(app/providers): atualizar arquivo com PlayerProvider

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,8 +1,13 @@
 "use client"
 
+import { PlayerProvider } from "@/context/PlayerContext";
 import { SessionProvider } from "next-auth/react";
 import { ReactNode } from "react";
 
 export default function Providers({ children }: { children: ReactNode }) {
-    return <SessionProvider>{children}</SessionProvider>;
+    return (
+    <SessionProvider>
+        <PlayerProvider>{children}</PlayerProvider>
+    </SessionProvider>
+    );
 }
