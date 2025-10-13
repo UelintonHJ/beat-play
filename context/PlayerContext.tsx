48f2b02fd9801570df8fa82cef410451000f40c0
Uelinton Janke <uelinton.h@gmail.com>
2025-10-13 20:56:16 -0300

    feat(context/PlayerContext): passar token para o contexto

--- context/PlayerContext.tsx
+++ context/PlayerContext.tsx
@@ -7,7 +7,7 @@ interface PlayerContextType {
     currentTrack: Track | null;
     setCurrentTrack: (track: Track) => void;
     playTrack: (trackId: string) => void;
-    setDevice: (deviceId: string) => void;
+    setDevice: (deviceId: string, token: string) => void;
 }
 
 const PlayerContext = createContext<PlayerContextType>({
@@ -22,9 +22,10 @@ export const PlayerProvider = ({ children }: { children: ReactNode }) => {
     const [deviceId, setDeviceId] = useState<string | null>(null);
     const [token, setToken] = useState<string | null>(null);
 
-    const setDevice = (device: string) => {
+    const setDevice = (device: string, token: string) => {
         setDeviceId(device);
-    }
+        setToken(token);
+    };
 
     const playTrack = async (trackId: string) => {
         if (!deviceId || !token) {
