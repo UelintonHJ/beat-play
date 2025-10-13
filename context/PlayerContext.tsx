54896c22669cb3dfa8c66215646cb16ffb96c1e9
Uelinton Janke <uelinton.h@gmail.com>
2025-10-13 19:51:34 -0300

    feat(context/PlayerContext): add playTrack

--- context/PlayerContext.tsx
+++ context/PlayerContext.tsx
@@ -6,18 +6,45 @@ import { createContext, ReactNode, useState, useContext } from "react";
 interface PlayerContextType {
     currentTrack: Track | null;
     setCurrentTrack: (track: Track) => void;
+    playTrack: (trackId: string) => void;
+    setDevice: (deviceId: string, token: string) => void;
 }
 
 const PlayerContext = createContext<PlayerContextType>({
     currentTrack: null,
     setCurrentTrack: () => {},
+    playTrack: () => {},
+    setDevice: () => {},
 });
 
 export const PlayerProvider = ({ children }: { children: ReactNode }) => {
     const [currentTrack, setCurrentTrack] = useState<Track | null>(null);
+    const [deviceId, setDeviceId] = useState<string | null>(null);
+    const [token, setToken] = useState<string | null>(null);
+
+    const setDevice = (device: string, token: string) => {
+        setDeviceId(device);
+        setToken(token);
+    }
+
+    const playTrack = async (trackId: string) => {
+        if (!deviceId || !token) return;
+        try {
+            await fetch(`https://api.spotify.com/v1/me/player/play?device_id=${deviceId}`, {
+                method: "PUT",
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ uris: [`spotify:track:${trackId}`] }),
+            });
+        } catch (err) {
+            console.error("Error ao tocar música:", err);
+        }
+    };
 
     return (
-        <PlayerContext.Provider value={{ currentTrack, setCurrentTrack }}>
+        <PlayerContext.Provider value={{ currentTrack, setCurrentTrack, playTrack, setDevice }}>
             {children}
         </PlayerContext.Provider>
     );
